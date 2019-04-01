c215f5397fe834c53e21595ab7fb7ac28973ca7e
Masihullah17 <masihulla17@gmail.com>
2019-04-02 03:38:05 +0530

    Assets

---
+++ Assets/scoreScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreScript : MonoBehaviour
+{
+    public static int enemiesLeft = 6;
+    Text enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemies.text = "Enemies Left : " + enemiesLeft;
+    }
+}
